dc502709254c005e77ea2127c722dbba351260bb
Krzysiek <krzykop@gmail.com>
2020-12-17 23:32:23 +0100

    new stages and repair code

---
+++ app.js
@@ -0,0 +1,13 @@
+var stage2 = "stage2.html"
+	
+	function startfun() 
+	{
+            alert("Witaj Graczu")
+			document.location.href = stage1;
+	
+    }
+    function next_page()
+{
+	var stage_end = "stage_end.html"
+	document.location.href = stage_end;
+}
