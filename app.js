378dede8fc5c7351eedf7930f3e8d091ad3dc7f1
Krzysiek <krzykop@gmail.com>
2020-12-19 11:34:01 +0100

    repair bad files and add two part of the end

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var stage2 = "stage2.html"
-	
+var stageBomb= "bomb.html"
+var mainpage="main.html"
+var stage_end_fail = "stage_end_failed.html"
+var stage_end_pos = "stage_positive.html"; ///brak
+var stage_driving_train="train.html"   // brak
+	function backtomainpage()
+	{
+		document.location.href = mainpage;
+	}	
 	function startfun() 
 	{
-            alert("Witaj Graczu")
-			document.location.href = stage1;
+           // alert("Witaj Graczu")
+		   document.location.href = stage2;
 	
     }
-    function next_page()
+    function next_page() //stage2
 {
-	var stage_end = "stage_end.html"
-	document.location.href = stage_end;
+	document.location.href = stageBomb;
 }
+function  failed(){ document.location.href= stage_end_fail }
+function  positive(){  document.location.href = stage_end_pos; }
