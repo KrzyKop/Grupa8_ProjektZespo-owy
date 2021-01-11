c0ac94e4304e512f4743e687640d542b27129907
Krzysiek <krzykop@gmail.com>
2021-01-11 16:14:34 +0100

    finished stage 2 with mistake: without new line

--- app.js
+++ app.js
@@ -136,3 +136,23 @@ function addcounter()
 			failed()
 		}
 	}
+// Wrap every letter in a span
+
+  $('.ml1 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  var ml = { timelines: {}};
+  ml.timelines["ml1"] = anime.timeline({loop: false})
+    .add({
+      targets: '.letter',
+      scale: [0.3,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 100,
+      delay: function(el, i) {
+        return 70 * (i+1)
+      }
+    })
+
+	
