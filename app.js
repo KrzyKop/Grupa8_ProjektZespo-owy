aaf361de47f91e7b1dae54cd0d14bcfda73e425c
Krzysiek <krzykop@gmail.com>
2021-01-16 19:49:55 +0100

    poprawiony main

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var stage8 = "stage8.html" // train + przejscie
 var stage9 = "stage9.html" // schemat
 var stage10 = "stage10.html" //bomba
 
+var stage_end_fail = "stage_end_failed.html"
+var stage_end_pos = "stage_end_pos.html"; 
 
-var password= [20]
-for(var i=0; i<21;i++)
-{
-	password[i]=0
-}
+
+var password= [10]
+
 password[1]=stage2;
 password[2]=stage3;
 password[3]=stage4;
@@ -22,28 +22,51 @@ password[4]=stage5;
 password[5]=stage6;
 password[6]=stage7;
 password[7]=stage8;
-password[8]=stage9;
-password[9]=stage10;
-password[6+10]="pomocy bomba!"
+password[8]="x";
+password[9]="x";
 
 
-var stage_end_fail = "stage_end_failed.html"
-var stage_end_pos_b = "stage_positive_a.html"; ///brak
-var stage_end_pos_b = "stage_positive_b.html"; ///brak
+
 
 var counter =0;
 var player=null;
-	
 
+function err_createElement(error)
+{
+	
+  newDiv = document.createElement("div");
+  newDiv.id="error_div"
+  newDiv.innerHTML = error;
+  newDiv.classList.add("error_div");
+  my_div = document.getElementById("error_div");
+  document.body.insertBefore(newDiv, my_div);
+  
+  var err_btn = document.createElement("div");
+  err_btn.id="error_btn"
+  err_btn.innerHTML = "X";
+ document.body.appendChild(err_btn);
+ err_btn.classList.add("err_btn"); 
+ err_btn.addEventListener("click", err_close);	
+
+  document.getElementById("text_button").disabled = true;
+}
+function err_close()
+{
+	var removed_div = document.getElementById("error_div");
+	var removed_btn = document.getElementById("error_btn");
+	
+	 removed_div.remove();
+	 removed_btn.remove();
+	///document.getElementById("btn").disabled = false;	 check it
+}
 function setplayer() 
 {
 	var decision=false;
-	tmp=document.getElementById('main_id').value
-	console.log(document.getElementById('main_id').value)
-	alert(tmp)
+	tmp=document.getElementById('main_textbox').value 
 	if(tmp==null || tmp=="")
 	{
-		alert("Prosze Podac nazwe gracza!")
+		var error="BŁĄD!!! <BR> PROSZĘ SPRAWDZIĆ WPROWADZONE DANE!!!" ;
+		err_createElement(error);
 		decision = false;
 	}
 	else
@@ -67,13 +90,11 @@ function setplayer()
 	}	
 	function startfun() 
 	{
-           alert("Witaj "+ player)
 		   document.location.href = stage2;	
 	}
 	
     function stage2_next() //stage2
 {
-	setstage(stage3);
 	document.location.href = stage3;
 }
 
@@ -118,18 +139,109 @@ function move_stage_train() //stage8a
 }
 function stage9_next() //stage9
 {
-	document.location.href = stage10;
+	var nextto=false;
+	var password_from_users = document.getElementById("stage9_input").value;
+	if(password_from_users==password[9])
+	{
+		nextto=true;
+	}
+	else
+	{
+		addcounter();
+	}
+	if(nextto==true)
+	{
+		document.location.href = stage10;
+	}
 }
 function stage10_next() //stage10
 {
 	document.location.href = stage10;
 }
 
+function question_final_V1() //stage 10 - pozytywne rozbrojenie bomby okienko
+{	
+	final = document.createElement("div");
+	final.id="final"
+	final.innerHTML = "ZESPÓŁ SAPERSKI: JESTES PEWIEN?";
+	final.classList.add("div_q");
+  	my_div1 = document.getElementById("final");
+  	document.body.insertBefore(final, my_div1)
 
+	btn1 = document.createElement("div");
+	btn1.id="final_no"
+	btn1.innerHTML = "NIE";
+	btn1.classList.add("final_no");
+	my_div2 = document.getElementById("final_no");
+	document.body.insertBefore(btn1, my_div2) 
 
+	btn2 = document.createElement("div");
+	btn2.id="final_yes"
+	btn2.innerHTML = "TAK";
+	btn2.classList.add("final_yes");
+	my_div3 = document.getElementById("final_yes");
+	document.body.insertBefore(btn2, my_div3)
 
+	btn1.addEventListener("click", question_final_close_no);	 //no 
+	btn2.addEventListener("click", question_finalv1_close);	 //yes
+}
+function question_final_V2() //stage 10 - negatywne rozbrojenie bomby okienko
+{	
+	final = document.createElement("div");
+	final.id="final"
+	final.innerHTML = "ZESPÓŁ SAPERSKI: JESTES PEWIEN?";
+	final.classList.add("div_q");
+  	my_div1 = document.getElementById("final");
+  	document.body.insertBefore(final, my_div1)
 
+	btn1 = document.createElement("div");
+	btn1.id="final_no"
+	btn1.innerHTML = "NIE";
+	btn1.classList.add("final_no");
+	my_div2 = document.getElementById("final_no");
+	document.body.insertBefore(btn1, my_div2) 
 
+	btn2 = document.createElement("div");
+	btn2.id="final_yes"
+	btn2.innerHTML = "TAK";
+	btn2.classList.add("final_yes");
+	my_div3 = document.getElementById("final_yes");
+	document.body.insertBefore(btn2, my_div3)
+
+	btn1.addEventListener("click", question_final_close_no);	
+	btn2.addEventListener("click", question_finalv2_close);	
+}
+function question_final_close_no() //zamkniecie okienka 
+{
+	var removed_div = document.getElementById("final");
+	var removed_btn1 = document.getElementById("final_yes");
+	var removed_btn2 = document.getElementById("final_no");
+	 removed_div.remove();
+	 removed_btn1.remove();
+	 removed_btn2.remove();
+}
+function question_finalv2_close() //negatywne rozbrojenie - stage 10  failed
+{
+	var removed_div = document.getElementById("final");
+	var removed_btn1 = document.getElementById("final_yes");
+	var removed_btn2 = document.getElementById("final_no");
+	
+	 removed_div.remove();
+	 removed_btn1.remove();
+	 removed_btn2.remove();
+	 failed();
+}
+function question_finalv1_close() //negatywne rozbrojenie - stage 10  positive
+{
+	var removed_div = document.getElementById("final");
+	var removed_btn1 = document.getElementById("final_yes");
+	var removed_btn2 = document.getElementById("final_no");
+	
+	 removed_div.remove();
+	 removed_btn1.remove();
+	 removed_btn2.remove();
+	 positive();
+}
 
 function  failed(){ document.location.href= stage_end_fail }
 function  positive(){  document.location.href = stage_end_pos; }
@@ -158,15 +270,22 @@ function addcounter()
 		counter++
 		checkcounter()
 	}
-	function printCounter(){alert("counter: "+ counter)}
+	function printCounter(){console.log("counter: "+ counter)}
 	function checkcounter()
 	{
 		printCounter()
-		if(counter >3)
+		if(counter <4)
 		{
-			alert("przekroczono ilosc mozliwych bledow!")
+			var error="Błedne hasło!!!";
+				err_createElement(error);
+				console.log("cccc")
+				
+				
+		}else if(counter==4)
+		{
 			failed()
 		}
+		else{console.log("error")}
 	}
  /*
   $('.ml1 .letters').each(function(){
@@ -205,5 +324,6 @@ function changeimagefour()
 function changeimagefive()
 {
 	document.body.style.backgroundImage ="url('img/schemat_pociagu_wagon5.jpeg')";
+
 }
 		
